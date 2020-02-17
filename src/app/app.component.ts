3fc52ac82eb4e33c8dcbdb8084449dae0fc246f0
SALEH ADAM <G00374853@gmit.ie>
2020-02-17 18:07:41 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'data-binding-app';
+
+  presidents: string[] = ["iggins", "McAleese", "Robinson", "Hillery", "O Dalaigh"];
+
 }
